591154ad50cef9690b0f05f024c4d6ae126cdc7e
Sergey <shornikov1407@gmail.com>
2025-10-16 12:30:52 +0300

    Implement messenger: server with multithreading and client with message mode

--- client.cpp
+++ client.cpp
@@ -1,84 +1,296 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include <arpa/inet.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <termios.h>
 #include <unistd.h>
 
-#include <string.h>
+#include <string>
 
-int main(int argc, char *argv[]) {
-  (void)argc;
-  (void)argv;
-  int sockfd, n;
-  uint16_t portno;
-  struct sockaddr_in serv_addr;
-  struct hostent *server;
+struct ClientData {
+  int socket;
+  std::string nickname;
+  bool running;
+  bool input_mode;
+  pthread_mutex_t print_mutex;
+};
 
-  char buffer[256];
+ClientData client_data;
 
-  if (argc < 3) {
-    fprintf(stderr, "usage %s hostname port\n", argv[0]);
-    exit(0);
-  }
+void* receive_messages(void* arg);
+void send_message(int socket, const char* nickname, const char* message);
+void set_terminal_mode(bool raw);
+void clear_line();
 
-  portno = (uint16_t)atoi(argv[2]);
-
-  /* Create a socket point */
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-  if (sockfd < 0) {
-    perror("ERROR opening socket");
+int main(int argc, char* argv[]) {
+  if (argc < 4) {
+    fprintf(stderr, "Usage: %s <server_address> <port> <nickname>\n", argv[0]);
     exit(1);
   }
 
-  server = gethostbyname(argv[1]);
+  const char* server_address = argv[1];
+  const char* port_str = argv[2];
+  const char* nickname = argv[3];
 
-  if (server == NULL) {
-    fprintf(stderr, "ERROR, no such host\n");
-    exit(0);
-  }
+  client_data.nickname = nickname;
+  client_data.running = true;
+  client_data.input_mode = false;
+  pthread_mutex_init(&client_data.print_mutex, NULL);
 
-  bzero((char *)&serv_addr, sizeof(serv_addr));
-  serv_addr.sin_family = AF_INET;
-  bcopy(server->h_addr, (char *)&serv_addr.sin_addr.s_addr,
-        (size_t)server->h_length);
-  serv_addr.sin_port = htons(portno);
+  // Resolve address (supports both IPv4 and IPv6)
+  struct addrinfo hints, *result, *rp;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_UNSPEC;      // Allow IPv4 or IPv6
+  hints.ai_socktype = SOCK_STREAM;  // TCP socket
 
-  /* Now connect to the server */
-  if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-    perror("ERROR connecting");
+  int status = getaddrinfo(server_address, port_str, &hints, &result);
+  if (status != 0) {
+    fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
     exit(1);
   }
 
-  /* Now ask for a message from the user, this message
-   * will be read by server
-   */
+  // Try each address until we successfully connect
+  int sockfd = -1;
+  for (rp = result; rp != NULL; rp = rp->ai_next) {
+    sockfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+    if (sockfd == -1) continue;
 
-  printf("Please enter the message: ");
-  bzero(buffer, 256);
-  if (fgets(buffer, 255, stdin) == NULL) {
-    perror("ERROR reading from stdin");
+    if (connect(sockfd, rp->ai_addr, rp->ai_addrlen) != -1) {
+      break;  // Success
+    }
+
+    close(sockfd);
+    sockfd = -1;
+  }
+
+  freeaddrinfo(result);
+
+  if (sockfd == -1) {
+    fprintf(stderr, "Could not connect to server\n");
     exit(1);
   }
 
-  /* Send message to the server */
-  n = write(sockfd, buffer, strlen(buffer));
+  client_data.socket = sockfd;
+  printf("Connected to server as '%s'\n", nickname);
+  printf(
+      "Press 'm' to enter message mode, then type your message and press "
+      "Enter to send.\n");
+  printf("Press Ctrl+C to exit.\n\n");
 
-  if (n < 0) {
-    perror("ERROR writing to socket");
+  // Create receive thread
+  pthread_t recv_thread;
+  if (pthread_create(&recv_thread, NULL, receive_messages, NULL) != 0) {
+    perror("ERROR creating receive thread");
+    close(sockfd);
     exit(1);
   }
 
-  /* Now read server response */
-  bzero(buffer, 256);
-  n = read(sockfd, buffer, 255);
+  // Set terminal to raw mode for character-by-character input
+  set_terminal_mode(true);
 
-  if (n < 0) {
-    perror("ERROR reading from socket");
-    exit(1);
+  // Main input loop
+  std::string input_buffer;
+  char ch;
+  while (client_data.running) {
+    if (read(STDIN_FILENO, &ch, 1) <= 0) {
+      break;
+    }
+
+    if (!client_data.input_mode) {
+      // Not in input mode - wait for 'm' key
+      if (ch == 'm' || ch == 'M') {
+        client_data.input_mode = true;
+        pthread_mutex_lock(&client_data.print_mutex);
+        printf("\r[Message mode] > ");
+        fflush(stdout);
+        pthread_mutex_unlock(&client_data.print_mutex);
+        input_buffer.clear();
+      }
+    } else {
+      // In input mode
+      if (ch == '\n' || ch == '\r') {
+        // Send message
+        if (!input_buffer.empty()) {
+          pthread_mutex_lock(&client_data.print_mutex);
+          printf("\r");
+          clear_line();
+          fflush(stdout);
+          pthread_mutex_unlock(&client_data.print_mutex);
+
+          send_message(sockfd, nickname, input_buffer.c_str());
+          input_buffer.clear();
+        }
+        client_data.input_mode = false;
+      } else if (ch == 127 || ch == 8) {
+        // Backspace
+        if (!input_buffer.empty()) {
+          input_buffer.pop_back();
+          pthread_mutex_lock(&client_data.print_mutex);
+          printf("\b \b");
+          fflush(stdout);
+          pthread_mutex_unlock(&client_data.print_mutex);
+        }
+      } else if (ch >= 32 && ch < 127) {
+        // Printable character
+        input_buffer += ch;
+        pthread_mutex_lock(&client_data.print_mutex);
+        printf("%c", ch);
+        fflush(stdout);
+        pthread_mutex_unlock(&client_data.print_mutex);
+      }
+    }
   }
 
-  printf("%s\n", buffer);
+  // Cleanup
+  set_terminal_mode(false);
+  client_data.running = false;
+  close(sockfd);
+  pthread_join(recv_thread, NULL);
+  pthread_mutex_destroy(&client_data.print_mutex);
+
+  printf("\nDisconnected from server.\n");
   return 0;
+}
+
+void* receive_messages(void*) {
+  int socket = client_data.socket;
+
+  while (client_data.running) {
+    // Read nickname size
+    uint32_t nickname_size_net;
+    ssize_t n = recv(socket, &nickname_size_net, 4, MSG_WAITALL);
+    if (n <= 0) {
+      break;
+    }
+    uint32_t nickname_size = ntohl(nickname_size_net);
+
+    if (nickname_size == 0 || nickname_size > 1024) {
+      fprintf(stderr, "Invalid nickname size received\n");
+      break;
+    }
+
+    // Read nickname
+    char* nickname = new char[nickname_size + 1];
+    n = recv(socket, nickname, nickname_size, MSG_WAITALL);
+    if (n <= 0) {
+      delete[] nickname;
+      break;
+    }
+    nickname[nickname_size] = '\0';
+
+    // Read body size
+    uint32_t body_size_net;
+    n = recv(socket, &body_size_net, 4, MSG_WAITALL);
+    if (n <= 0) {
+      delete[] nickname;
+      break;
+    }
+    uint32_t body_size = ntohl(body_size_net);
+
+    if (body_size > 65536) {
+      fprintf(stderr, "Invalid body size received\n");
+      delete[] nickname;
+      break;
+    }
+
+    // Read body
+    char* body = new char[body_size + 1];
+    n = recv(socket, body, body_size, MSG_WAITALL);
+    if (n <= 0) {
+      delete[] nickname;
+      delete[] body;
+      break;
+    }
+    body[body_size] = '\0';
+
+    // Read date size
+    uint32_t date_size_net;
+    n = recv(socket, &date_size_net, 4, MSG_WAITALL);
+    if (n <= 0) {
+      delete[] nickname;
+      delete[] body;
+      break;
+    }
+    uint32_t date_size = ntohl(date_size_net);
+
+    if (date_size > 100) {
+      fprintf(stderr, "Invalid date size received\n");
+      delete[] nickname;
+      delete[] body;
+      break;
+    }
+
+    // Read date
+    char* date = new char[date_size + 1];
+    n = recv(socket, date, date_size, MSG_WAITALL);
+    if (n <= 0) {
+      delete[] nickname;
+      delete[] body;
+      delete[] date;
+      break;
+    }
+    date[date_size] = '\0';
+
+    // Display message
+    pthread_mutex_lock(&client_data.print_mutex);
+    if (client_data.input_mode) {
+      // In input mode - print message above input line
+      printf("\r");
+      clear_line();
+      printf("{%s} [%s] %s\n", date, nickname, body);
+      printf("[Message mode] > ");
+      fflush(stdout);
+    } else {
+      // Not in input mode - just print
+      printf("{%s} [%s] %s\n", date, nickname, body);
+      fflush(stdout);
+    }
+    pthread_mutex_unlock(&client_data.print_mutex);
+
+    delete[] nickname;
+    delete[] body;
+    delete[] date;
+  }
+
+  client_data.running = false;
+  return NULL;
+}
+
+void send_message(int socket, const char* nickname, const char* message) {
+  // Send nickname size and nickname
+  uint32_t nickname_size = strlen(nickname);
+  uint32_t nickname_size_net = htonl(nickname_size);
+  send(socket, &nickname_size_net, 4, MSG_NOSIGNAL);
+  send(socket, nickname, nickname_size, MSG_NOSIGNAL);
+
+  // Send message size and message
+  uint32_t message_size = strlen(message);
+  uint32_t message_size_net = htonl(message_size);
+  send(socket, &message_size_net, 4, MSG_NOSIGNAL);
+  send(socket, message, message_size, MSG_NOSIGNAL);
+}
+
+void set_terminal_mode(bool raw) {
+  static struct termios old_termios;
+
+  if (raw) {
+    // Save old settings
+    tcgetattr(STDIN_FILENO, &old_termios);
+
+    // Set raw mode
+    struct termios new_termios = old_termios;
+    new_termios.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_termios);
+  } else {
+    // Restore old settings
+    tcsetattr(STDIN_FILENO, TCSANOW, &old_termios);
+  }
+}
+
+void clear_line() {
+  printf("\033[2K");  // Clear entire line
 }
--- server.cpp
+++ server.cpp
@@ -1,77 +1,299 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-#include <netdb.h>
-#include <netinet/in.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <time.h>
 #include <unistd.h>
 
-#include <string.h>
+#include <vector>
 
-int main(int argc, char *argv[]) {
-  (void)argc;
-  (void)argv;
-  int sockfd, newsockfd;
-  uint16_t portno;
-  unsigned int clilen;
-  char buffer[256];
-  struct sockaddr_in serv_addr, cli_addr;
-  ssize_t n;
+#define MAX_CLIENTS 100
 
-  /* First call to socket() function */
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+struct Client {
+  int socket;
+  pthread_t thread;
+  bool active;
+};
 
-  if (sockfd < 0) {
+struct ServerData {
+  std::vector<Client*> clients;
+  pthread_mutex_t clients_mutex;
+};
+
+ServerData server_data;
+
+void* handle_client(void* arg);
+void broadcast_message(const char* nickname, uint32_t nickname_size,
+                       const char* body, uint32_t body_size, int);
+void remove_client(int socket);
+
+int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+    exit(1);
+  }
+
+  uint16_t port = (uint16_t)atoi(argv[1]);
+  printf("Starting server on port %d...\n", port);
+  fflush(stdout);
+
+  // Initialize mutex
+  pthread_mutex_init(&server_data.clients_mutex, NULL);
+  printf("Mutex initialized\n");
+  fflush(stdout);
+
+  // Create socket with IPv6 (supports both IPv4 and IPv6)
+  int server_socket = socket(AF_INET6, SOCK_STREAM, 0);
+  if (server_socket < 0) {
     perror("ERROR opening socket");
     exit(1);
   }
+  printf("Socket created\n");
+  fflush(stdout);
 
-  /* Initialize socket structure */
-  bzero((char *)&serv_addr, sizeof(serv_addr));
-  portno = 5001;
+  // Set socket options
+  int opt = 1;
+  if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) <
+      0) {
+    perror("ERROR setsockopt SO_REUSEADDR");
+    close(server_socket);
+    exit(1);
+  }
 
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_addr.s_addr = INADDR_ANY;
-  serv_addr.sin_port = htons(portno);
+  // Enable dual-stack (IPv4 and IPv6)
+  int ipv6only = 0;
+  if (setsockopt(server_socket, IPPROTO_IPV6, IPV6_V6ONLY, &ipv6only,
+                 sizeof(ipv6only)) < 0) {
+    perror("ERROR setsockopt IPV6_V6ONLY");
+    close(server_socket);
+    exit(1);
+  }
 
-  /* Now bind the host address using bind() call.*/
-  if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+  // Bind socket
+  struct sockaddr_in6 server_addr;
+  memset(&server_addr, 0, sizeof(server_addr));
+  server_addr.sin6_family = AF_INET6;
+  server_addr.sin6_addr = in6addr_any;
+  server_addr.sin6_port = htons(port);
+
+  if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) <
+      0) {
     perror("ERROR on binding");
+    close(server_socket);
     exit(1);
   }
+  printf("Socket bound to port %d\n", port);
+  fflush(stdout);
 
-  /* Now start listening for the clients, here process will
-   * go in sleep mode and will wait for the incoming connection
-   */
-
-  listen(sockfd, 5);
-  clilen = sizeof(cli_addr);
-
-  /* Accept actual connection from the client */
-  newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
-
-  if (newsockfd < 0) {
-    perror("ERROR on accept");
+  // Listen for connections
+  if (listen(server_socket, MAX_CLIENTS) < 0) {
+    perror("ERROR on listen");
+    close(server_socket);
     exit(1);
   }
+  printf("Listening for connections...\n");
+  fflush(stdout);
 
-  /* If connection is established then start communicating */
-  bzero(buffer, 256);
-  n = read(newsockfd, buffer, 255);
+  printf("Server listening on port %d (IPv4 and IPv6)\n", port);
+  fflush(stdout);
 
-  if (n < 0) {
-    perror("ERROR reading from socket");
-    exit(1);
+  // Accept loop
+  while (true) {
+    struct sockaddr_in6 client_addr;
+    socklen_t client_len = sizeof(client_addr);
+
+    int client_socket =
+        accept(server_socket, (struct sockaddr*)&client_addr, &client_len);
+    if (client_socket < 0) {
+      perror("ERROR on accept");
+      continue;
+    }
+
+    printf("New client connected (socket %d)\n", client_socket);
+    fflush(stdout);
+
+    // Create client structure
+    Client* client = new Client();
+    client->socket = client_socket;
+    client->active = true;
+
+    // Add to clients list
+    pthread_mutex_lock(&server_data.clients_mutex);
+    server_data.clients.push_back(client);
+    pthread_mutex_unlock(&server_data.clients_mutex);
+
+    // Create thread for client
+    if (pthread_create(&client->thread, NULL, handle_client, (void*)client) !=
+        0) {
+      perror("ERROR creating thread");
+      pthread_mutex_lock(&server_data.clients_mutex);
+      for (auto it = server_data.clients.begin();
+           it != server_data.clients.end(); ++it) {
+        if (*it == client) {
+          server_data.clients.erase(it);
+          break;
+        }
+      }
+      pthread_mutex_unlock(&server_data.clients_mutex);
+      close(client_socket);
+      delete client;
+    }
   }
 
-  printf("Here is the message: %s\n", buffer);
-
-  /* Write a response to the client */
-  n = write(newsockfd, "I got your message", 18);
-
-  if (n < 0) {
-    perror("ERROR writing to socket");
-    exit(1);
-  }
-
+  close(server_socket);
+  pthread_mutex_destroy(&server_data.clients_mutex);
   return 0;
+}
+
+void* handle_client(void* arg) {
+  Client* client = (Client*)arg;
+  int socket = client->socket;
+
+  while (client->active) {
+    // Read nickname size
+    uint32_t nickname_size_net;
+    ssize_t n = recv(socket, &nickname_size_net, 4, MSG_WAITALL);
+    if (n <= 0) {
+      break;
+    }
+    uint32_t nickname_size = ntohl(nickname_size_net);
+
+    if (nickname_size == 0 || nickname_size > 1024) {
+      fprintf(stderr, "Invalid nickname size: %u\n", nickname_size);
+      break;
+    }
+
+    // Read nickname
+    char* nickname = new char[nickname_size + 1];
+    n = recv(socket, nickname, nickname_size, MSG_WAITALL);
+    if (n <= 0) {
+      delete[] nickname;
+      break;
+    }
+    nickname[nickname_size] = '\0';
+
+    // Read body size
+    uint32_t body_size_net;
+    n = recv(socket, &body_size_net, 4, MSG_WAITALL);
+    if (n <= 0) {
+      delete[] nickname;
+      break;
+    }
+    uint32_t body_size = ntohl(body_size_net);
+
+    if (body_size > 65536) {
+      fprintf(stderr, "Invalid body size: %u\n", body_size);
+      delete[] nickname;
+      break;
+    }
+
+    // Read body
+    char* body = new char[body_size + 1];
+    n = recv(socket, body, body_size, MSG_WAITALL);
+    if (n <= 0) {
+      delete[] nickname;
+      delete[] body;
+      break;
+    }
+    body[body_size] = '\0';
+
+    printf("Message from %s: %s\n", nickname, body);
+    fflush(stdout);
+
+    // Broadcast message to all clients
+    broadcast_message(nickname, nickname_size, body, body_size, socket);
+
+    delete[] nickname;
+    delete[] body;
+  }
+
+  printf("Client disconnected (socket %d)\n", socket);
+  fflush(stdout);
+  remove_client(socket);
+  close(socket);
+  return NULL;
+}
+
+void broadcast_message(const char* nickname, uint32_t nickname_size,
+                       const char* body, uint32_t body_size, int) {
+  // Get current time
+  time_t now = time(NULL);
+  struct tm* tm_info = localtime(&now);
+  char time_str[32];
+  strftime(time_str, sizeof(time_str), "%H:%M", tm_info);
+  uint32_t date_size = strlen(time_str);
+
+  // Prepare message to send
+  // [nickname_size][nickname][body_size][body][date_size][date]
+  uint32_t nickname_size_net = htonl(nickname_size);
+  uint32_t body_size_net = htonl(body_size);
+  uint32_t date_size_net = htonl(date_size);
+
+  pthread_mutex_lock(&server_data.clients_mutex);
+
+  for (auto it = server_data.clients.begin(); it != server_data.clients.end();
+       ++it) {
+    Client* client = *it;
+    if (!client->active) continue;
+
+    // Send to all clients including sender
+    ssize_t sent = 0;
+
+    sent = send(client->socket, &nickname_size_net, 4, MSG_NOSIGNAL);
+    if (sent <= 0) {
+      client->active = false;
+      continue;
+    }
+
+    sent = send(client->socket, nickname, nickname_size, MSG_NOSIGNAL);
+    if (sent <= 0) {
+      client->active = false;
+      continue;
+    }
+
+    sent = send(client->socket, &body_size_net, 4, MSG_NOSIGNAL);
+    if (sent <= 0) {
+      client->active = false;
+      continue;
+    }
+
+    sent = send(client->socket, body, body_size, MSG_NOSIGNAL);
+    if (sent <= 0) {
+      client->active = false;
+      continue;
+    }
+
+    sent = send(client->socket, &date_size_net, 4, MSG_NOSIGNAL);
+    if (sent <= 0) {
+      client->active = false;
+      continue;
+    }
+
+    sent = send(client->socket, time_str, date_size, MSG_NOSIGNAL);
+    if (sent <= 0) {
+      client->active = false;
+      continue;
+    }
+  }
+
+  pthread_mutex_unlock(&server_data.clients_mutex);
+}
+
+void remove_client(int socket) {
+  pthread_mutex_lock(&server_data.clients_mutex);
+
+  for (auto it = server_data.clients.begin(); it != server_data.clients.end();
+       ++it) {
+    if ((*it)->socket == socket) {
+      (*it)->active = false;
+      // Don't delete or join here - let the thread finish naturally
+      server_data.clients.erase(it);
+      break;
+    }
+  }
+
+  pthread_mutex_unlock(&server_data.clients_mutex);
 }
